034efb73203053bdf86b330b46883df493ab14e1
Sergey Myasnikov <gnom2@bk.ru>
2021-11-13 11:39:18 +0300

    beta 27 home

--- MSG/app/Http/Controllers/Auth/RegisterController.php
+++ MSG/app/Http/Controllers/Auth/RegisterController.php
@@ -66,7 +66,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
-            'last_name' => '555',
+            //'last_name' => '555',
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
