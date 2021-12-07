dc8586e1a6481a4646dd82378b52a19ceabf1170
Sergey Miasnikov <gnom2@bk.ru>
2021-12-07 10:05:05 +0300

    beta 79

--- MSG/app/Http/Middleware/Authenticate.php
+++ MSG/app/Http/Middleware/Authenticate.php
@@ -15,7 +15,9 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('login');
+
+          dd($request);
+          //  return route('login');
             //return $request->expectsJson();
         }
     }
